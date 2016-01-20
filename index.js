9f51fbfe899effd6f7f85e8dad67aa7892fd8407
Ben Tesser <btesser@rapid7.com>
2016-01-20 17:19:40 -0500

    more fixes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 module.exports = {
   rules: {
     'static-screaming-snake': require('./lib/rules/static-screaming-snake'),
-    'static-magic-number': require('./lib/rules/static-magic-numbers')
+    'static-magic-numbers': require('./lib/rules/static-magic-numbers')
   },
   rulesConfig: {
-    'magic-num': [2, {ignore: [], detectObjects: false, enforceConst: false, allowStatic: false}],
+    'static-magic-numbers': [2, {ignore: [], detectObjects: false, enforceConst: false, allowStatic: false}],
     'static-screaming-snake': [2]
   }
 };
