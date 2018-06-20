644fbdf8e3bae3b85060a799f4521abefe40d807
Tony Quetano <tony_quetano@rapid7.com>
2018-06-20 08:53:33 -0400

    Update for v6

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-'use strict';
-
 module.exports = {
   rules: {
-    'static-screaming-snake': require('./lib/rules/static-screaming-snake'),
-    'static-magic-numbers': require('./lib/rules/static-magic-numbers')
+    'object-property-newline': require('./lib/rules/object-property-newline'),
   },
-  rulesConfig: {
-    'static-magic-numbers': [2, {ignore: [], detectObjects: false, enforceConst: false, allowStatic: false}],
-    'static-screaming-snake': [2]
-  }
 };
