bb1830915ec541153c0bbafefe4a97af978a76a3
Tony Quetano <tony_quetano@rapid7.com>
2018-06-25 11:13:59 -0400

    Create rule specific to trailing underscores
    Basically a clone of `no-dangling-underscores` while allowing leading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'named-import-newline': require('./lib/rules/named-import-newline'),
+    'no-trailing-underscore': require('./lib/rules/no-trailing-underscore'),
     'sort-object-keys': require('./lib/rules/sort-object-keys'),
   },
 };
