6c29f679a208f824acd67fbc0816b545ca377c4d
Tony Quetano <tony_quetano@rapid7.com>
2018-06-29 09:50:47 -0400

    Prevent attempted sort when computed properties are complex
    Also, add tests and improve the documentation for the unfixable cases

--- docs/sort-object-keys.md
+++ docs/sort-object-keys.md
@@ -27,6 +27,28 @@ const spreadObject = {
   quz: 'quz',
   baz: 'baz',
 };
+
+// Case-sensitive by default.
+const object = {
+  foo: 'foo',
+  Bar: 'bar',
+  baz: 'baz'
+};
+
+// Non-natural order by default.
+const object = {
+  1: 'one',
+  2: 'two',
+  10: 'ten',
+};
+
+// This rule checks computed properities as well.
+const bar = 'bar';
+const object = {
+  foo: 'foo',
+  baz: 'baz'
+  [bar]: 'bar',
+};
 ```
 
 Examples of **correct** code for this rule:
@@ -46,18 +68,81 @@ const spreadObject = {
   baz: 'baz',
   quz: 'quz',
 };
+
+// Case-sensitive by default.
+const object = {
+  Bar: 'bar',
+  foo: 'foo',
+  baz: 'baz'
+};
+
+// Non-natural order by default.
+const object = {
+  1: 'one',
+  10: 'ten',
+  2: 'two',
+};
+
+// This rule checks computed properities as well.
+const bar = 'bar';
+const object = {
+  foo: 'foo',
+  [bar]: 'bar',
+  baz: 'baz'
+};
 ```
 
-## Caveats
+## Options
+
+```javascript
+{
+  "rapid7/sort-object-keys": ["error", "asc", {caseSensitive: true, natural: false}]
+}
+```
+
+The first option is a `string` that represents the order, either `"asc"` (ascending) or `"desc"` (descending).
+
+The second option is an object when has 2 available properties:
+
+- `caseSensitive` => if `true`, enforce properties to be case-sensitive order. Default is `true`.
+- `natural` => if `true`, enforce properties to be in natural order. Default is `false`.
+  + Natural Order compares strings containing combination of letters and numbers in the way a human being would sort. It basically sorts numerically, instead of sorting alphabetically.
+    + With `natural` as `true`, ordering would be `[1, 3, 6, 8, 10]`
+    + WIth `natural` as `false`, ordering would be `[1, 10, 3, 6, 8]`
+
+## Unfixable cases
 
 When there are comments declared within the context of the object, the rule will report problems but will not be able to autofix the issue.
 
 ```javascript
-const standardObject = {
+const object = {
   foo: 'foo',
   // this comment will cause the fixer to fail
   bar: 'bar',
   baz: 'baz',
+};
+```
+
+When there are complex computed properties that are assigned to the object, the rule will report problems but will not be able to autofix the issue.
+
+```javascript
+const object = {
+  foo: 'foo',
+  [bar.baz]: 'bar-baz',
+  quz: 'quz',
+};
+```
+
+Examples of what are considered "complex computed properties":
+
+```javascript
+const object = {
+  [a + b]: 'computation in key',
+  [`${c}.${d}`]: 'template literals',
+  [foo.bar.baz]: 'deeply-nested object properties',
+  [quz[1]]: 'array values',
+  [Symbol('e')]: 'symbols',
+  [tagged`f`]: 'tagged template literals',
 };
 ```
 
---
+++ test/rules/sort-object-keys.js
@@ -0,0 +1,415 @@
+// test
+const test = require('ava');
+const {RuleTester} = require('eslint');
+
+const ruleTester = new RuleTester({
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+});
+
+// src
+const rule = require('lib/rules/sort-object-keys');
+
+test('if the rule will validate the basic usage', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  baz: 'baz',
+  foo: 'foo',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  foo: 'foo',
+  baz: 'baz',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'baz' should be before 'foo'.",
+            },
+          ],
+          output: `const object = {
+  bar: 'bar',
+baz: 'baz',
+foo: 'foo',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate the numeric key usage', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  1: 'baz', 
+  2: 'foo',
+  3: 'bar',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  3: 'bar',
+  1: 'baz',
+  2: 'foo',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. '1' should be before '3'.",
+            },
+          ],
+          output: `const object = {
+  1: 'baz',
+2: 'foo',
+3: 'bar',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate the usage with comments but not fix them', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  1: 'baz',
+  // some comment
+  2: 'foo',
+  3: 'bar',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  3: 'bar',
+  // some comment
+  1: 'baz',
+  2: 'foo',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. '1' should be before '3'.",
+            },
+          ],
+          output: `const object = {
+  3: 'bar',
+  // some comment
+  1: 'baz',
+  2: 'foo',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate usage with computed properties', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  [baz]: 'baz',
+  foo: 'foo',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  foo: 'foo',
+  [baz]: 'baz',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'baz' should be before 'foo'.",
+            },
+          ],
+          output: `const object = {
+  bar: 'bar',
+[baz]: 'baz',
+foo: 'foo',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate usage with complex computed properties but not fix them', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  [array[1]]: 'baz',
+  bar: 'bar',
+  foo: 'foo',
+  [quz.blah]: 'quz',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  [quz.blah]: 'quz',
+  [array[1]]: 'baz',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'bar' should be before 'foo'.",
+            },
+          ],
+          output: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  [quz.blah]: 'quz',
+  [array[1]]: 'baz',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate usage with spread operations', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  foo: 'foo',
+  ...spread,
+  baz: 'baz', 
+  quz: 'quz',
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  ...spread,
+  quz: 'quz',
+  baz: 'baz',
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'bar' should be before 'foo'.",
+            },
+            {
+              message: "Expected object keys to be in ascending order. 'baz' should be before 'quz'.",
+            },
+          ],
+          output: `const object = {
+  bar: 'bar',
+foo: 'foo',
+...spread,
+baz: 'baz',
+quz: 'quz',
+...otherSpread,
+blah: 'blah',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate usage with spread operations and computed properties', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  foo: 'foo',
+  ...spread,
+  [baz]: 'baz', 
+  quz: 'quz',
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  ...spread,
+  quz: 'quz',
+  [baz]: 'baz',
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'bar' should be before 'foo'.",
+            },
+            {
+              message: "Expected object keys to be in ascending order. 'baz' should be before 'quz'.",
+            },
+          ],
+          output: `const object = {
+  bar: 'bar',
+foo: 'foo',
+...spread,
+[baz]: 'baz',
+quz: 'quz',
+...otherSpread,
+blah: 'blah',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate usage with spread operations and complex computed properties but not fix them', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  bar: 'bar',
+  foo: 'foo',
+  ...spread,
+  [baz]: 'baz', 
+  [quz.blah]: 'quz',
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  ...spread,
+  [quz.blah]: 'quz',
+  [baz]: 'baz', 
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in ascending order. 'bar' should be before 'foo'.",
+            },
+          ],
+          output: `const object = {
+  foo: 'foo',
+  bar: 'bar',
+  ...spread,
+  [quz.blah]: 'quz',
+  [baz]: 'baz', 
+  ...otherSpread,
+  blah: 'blah',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
+
+test('if the rule will validate the numeric key usage with natural ordering', t => {
+  try {
+    ruleTester.run('sort-object-keys', rule, {
+      valid: [
+        {
+          code: `const object = {
+  1: 'baz',
+  2: 'foo',
+  10: 'bar',
+}`.trim(),
+          options: ['asc', {natural: true}],
+        },
+      ],
+      invalid: [
+        {
+          code: `const object = {
+  1: 'baz',
+  10: 'bar',
+  2: 'foo',
+}`.trim(),
+          errors: [
+            {
+              message: "Expected object keys to be in natural ascending order. '2' should be before '10'.",
+            },
+          ],
+          options: ['asc', {natural: true}],
+          output: `const object = {
+  1: 'baz',
+2: 'foo',
+10: 'bar',
+}`.trim(),
+        },
+      ],
+    });
+
+    t.pass();
+  } catch (error) {
+    t.fail(error);
+  }
+});
